a9334235a65a1f1ee9ce810af64473664a917051
jhy <jianghuayu@popmart.com>
2024-10-23 16:48:48 +0800

    feat: model test

--- src/pages/Test/components/Model.js
+++ src/pages/Test/components/Model.js
@@ -5,11 +5,11 @@ import { MTLLoader } from "three/examples/jsm/loaders/MTLLoader";
 
 export default function Model() {
   // 加载 MTL 文件
-  const materials = useLoader(MTLLoader, "/bom.mtl"); // 替换为实际的 MTL 文件路径
+  const materials = useLoader(MTLLoader, "bom.mtl"); // 替换为实际的 MTL 文件路径
   materials.preload(); // 预加载材质
 
   // 加载 OBJ 文件
-  const obj = useLoader(OBJLoader, "/bom.obj", (loader) => {
+  const obj = useLoader(OBJLoader, "bom.obj", (loader) => {
     loader.setMaterials(materials); // 设置材质
   });
 
